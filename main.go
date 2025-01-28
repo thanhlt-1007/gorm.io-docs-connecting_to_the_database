3dd2e9d368b93c368c475b2df68c2500e847c685
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2025-01-28 15:20:10 +0700

    Add MYSQL_ variables

--- main.go
+++ main.go
@@ -7,10 +7,18 @@ import (
 )
 
 func main() {
-    dsn := "root:Aa@123456@tcp(127.0.0.1:3306)/db?charset=utf8mb4&parseTime=True&loc=Local"
+    MYSQL_USER := "root"
+    MYSQL_PASSWORD := "Aa@123456"
+    MYSQL_HOST := "127.0.0.1:3306"
+    MYSQL_DATABASE := "db"
+
+    dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+        MYSQL_USER, MYSQL_PASSWORD, MYSQL_HOST, MYSQL_DATABASE)
     _, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
     if err != nil {
         fmt.Printf("gorm.Open error: %v\n", err)
         panic(err)
     }
+
+    fmt.Println("gorm.Open success")
 }
